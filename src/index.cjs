aa7b8bd1ca51d83124014d057f7a5c307c90fccc
Aro Andriamaro <47597252+notAro14@users.noreply.github.com>
2024-02-27 21:14:57 +0100

    add route

--- src/index.cjs
+++ src/index.cjs
@@ -5,12 +5,15 @@ const serverOptions = {
 
 async function main() {
   const app = fastify(serverOptions);
+  app.get("/", async () => {
+    return { hello: "world" };
+  });
+
   await app.listen({
     port: 8080,
     host: "0.0.0.0",
   });
 
-  app.log.debug(app.initialConfig, "Fastify listening with the config");
   const port = app.server.address().port;
   app.log.info("HTTP Server port is %i", port);
 }
