ebdcc5396674119d533d06a3a7b909124a73741d
Aro Andriamaro <47597252+notAro14@users.noreply.github.com>
2024-05-05 11:21:20 +0200

    setup server for chapter 2

--- src/index.cjs
+++ src/index.cjs
@@ -1,67 +1,31 @@
-const fastify = require("fastify");
+const Fastify = require("fastify");
 const closeWithGrace = require("close-with-grace");
-const { Workout } = require("./workout.cjs");
+
 const serverOptions = {
   logger: { level: "info" },
 };
 if (process.stdout.isTTY)
   serverOptions.logger.transport = { target: "pino-pretty" };
 
-async function main() {
-  const app = fastify(serverOptions);
+const app = Fastify(serverOptions);
+app.get("/", async (request, reply) => {
+  return { hello: "world" };
+});
 
-  app.route({
-    url: "/",
-    method: "GET",
-    handler: async (req, reply) => {
-      reply.log.info("handler log");
-      app.log.info("app log");
-      return new Workout("My Workout");
-    },
-  });
-  const cats = [];
-  app.post("/cat", function saveCat(req, reply) {
-    cats.push(req.body);
-    reply.code(201).send({ allCats: cats });
-  });
-  app.get("/cat/:catName", function readCat(req, reply) {
-    const lookingFor = req.params.catName;
-    const result = cats.find((cat) => cat.name === lookingFor);
-    if (result) return { cat: result };
+app.listen({ port: 8080, host: "0.0.0.0" }, function (err, address) {
+  if (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
 
-    reply.code(404);
-    throw new Error(`cat ${lookingFor} not found`);
-  });
+  app.log.info(`server listening on ${address}`);
+});
 
-  await app.listen({
-    port: 8080,
-    host: "0.0.0.0",
-  });
-
-  const port = app.server.address().port;
-  app.log.info("HTTP Server port is %i", port);
-
-  closeWithGrace(async ({ signal, err }) => {
-    if (err) {
-      app.log.error({ err }, "server closing due to error");
-    } else {
-      app.log.info(`${signal} received, server closing`);
-    }
-    await app.close();
-  });
-
-  // ---- OR ----
-  // function closeApplication() {
-  //   app.close(function closeComplete(err) {
-  //     if (err) {
-  //       app.log.error(err, "error turning off");
-  //     } else {
-  //       app.log.info("bye bye");
-  //     }
-  //   });
-  // }
-  // process.on("SIGINT");
-  // process.on("SIGTERM", closeApplication);
-}
-
-main();
+closeWithGrace(async ({ signal, err }) => {
+  if (err) {
+    app.log.error({ err }, "server closing due to error");
+  } else {
+    app.log.info(`${signal} received, server closing`);
+  }
+  await app.close();
+});
